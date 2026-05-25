49f90eebdc8e87448173e0d35fafadb2235d928d
anaconex <62996555+anaconex@users.noreply.github.com>
2026-05-25 15:22:36 +0400

    Update

--- mh_booking_calendar/__manifest__.py
+++ mh_booking_calendar/__manifest__.py
@@ -21,6 +21,7 @@
         'sale_subscription', 'industry_fsm', 'appointment'
     ],
     'data': [
+        'security/security.xml',
         'security/ir.model.access.csv',
         'data/sequence_data.xml',
         'data/mail_template_data.xml',
